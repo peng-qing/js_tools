ef3ba8101bddaa3179971d29b7ace76f0d598d84
pengqq <ispromise886@163.com>
2024-08-16 01:19:10 +0800

    feat: :sparkles: random utils

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 "use strict";
 
-function main() {
+import { random_utils } from "./utils/random_utils.js";
 
+function main() {
+    const m1 = new Map();
+    m1.set(1, 1);
+
+    const m2 = new Map(m1);
+    m2.set(2, 100);
+
+    console.log(m1, m2);
+
+    const ret = random_utils.simpleWeightRandom(m2);
+
+    console.log(ret);
 }
 
 main();
---
+++ utils/random_utils.js
@@ -0,0 +1,102 @@
+"use strict";
+
+import { type_utils } from "./type_utils.js";
+
+export const random_utils = {};
+
+/**
+ * 获取随机数 左闭右闭 [min, max]
+ * @param {Number} min 
+ * @param {Number} max 
+ * @returns {Array}
+ */
+random_utils.getRandom = function (min, max) {
+    if (min >= max) {
+        [min, max] = [max, min];
+    }
+
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/**
+ * 打乱数组
+ * @param {Array} arr 
+ */
+random_utils.shuffleArray = function (arr) {
+    if (!type_utils.isArray(arr)) {
+        throw Error("[random_utils] shuffleArray args arr is not array");
+    }
+
+    arr.sort(() => {
+        return 0.5 - Math.random();
+    })
+}
+
+/**
+ * 不计算总权重的简单加权随机
+ * 根据Efraimidis和Spirakis在2006年发表的Paper：Weighted random sampling with a reservoir
+ * @param {Map} weightMap 
+ * @returns {any | null} 随机失败会返回 null
+ */
+random_utils.simpleWeightRandom = function (weightMap) {
+    if (!type_utils.isMap(weightMap)) {
+        throw Error("[random_utils] simpleWeightRandom invalid parameter input");
+    }
+
+    let maxKey = null, maxScore = Number.MIN_VALUE;
+    for (const [key, weight] of weightMap) {
+        // 0 权重不参与随机
+        if (weight === 0) {
+            continue;
+        }
+        // 计算每个key 对应的分数
+        // score = rand ^ (1.0/weight) rand为[0.0, 1.0]的随机
+        const score = Math.pow(Math.random(), 1.0 / weight);
+        if (score > maxScore) {
+            maxScore = score;
+            maxKey = key;
+        }
+    }
+
+    return maxKey;
+}
+
+/**
+ * 加权随机不重复随机
+ * @param {Map} weightMap key: 随机元素 value: 随机元素出现的权重值
+ * @param {Number} count 随机次数
+ * @returns {Array} 随机结果
+ */
+random_utils.getWeightRandom = function (weightMap, count) {
+    if (!type_utils.isMap(weightMap) || !type_utils.isNumber(count)) {
+        throw Error("[random_utils] getWeightRandom invalid parameter input");
+    }
+    // 空参
+    if (count <= 0 || weightMap.size <= 0) {
+        return [];
+    }
+    // 随机数量大于随机元素
+    if (weightMap.size <= count) {
+        return Array.from(weightMap.keys());
+    }
+    // 这里应该对 weightMap 进行深拷贝
+    const cpyWeight = new Map(weightMap);
+    const result = [];
+    for (let i = count; i > 0; i--) {
+        const key = random_utils.simpleWeightRandom(cpyWeight);
+        if (type_utils.isNull(key)) {
+            // null 说明随机失败 检查传入参数
+            throw Error(`[random_utils] getWeightRandom random reciprocal ${i} failed`);
+        }
+        if (!weightMap.has(key)) {
+            // 随机出不存在的key...
+            throw Error(`[random_utils] getWeightRandom random not exist key:${key} failed`);
+        }
+        // 不重复随机
+        cpyWeight.delete(key);
+        result.push(key);
+    }
+
+    return result;
+}
+
